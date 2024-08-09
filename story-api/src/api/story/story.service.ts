7775d23555f2ef68fac9acd44ed7e5c95e7861d8
datdev <datnm.ptit@gmail.com>
2024-08-09 09:48:41 +0700

    [story-api/fix/chart] Fixed the get chart data api.

--- story-api/src/api/story/story.service.ts
+++ story-api/src/api/story/story.service.ts
@@ -125,7 +125,7 @@ export class StoryService {
                         storyId: story.storyId
                     }
                 }))[0]
-                viewArr.push(viewCount.count)
+                viewArr.push(viewCount !== undefined ? viewCount.count : 0)
                 from += queries.step
             }
             data.push({
